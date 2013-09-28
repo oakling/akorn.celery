795e29aff633c9898e76b1ed215c03a2e051402c
Ubuntu <test@ubuntu>
2013-09-28 23:43:28 +0100

    New-style feed tasks

--- akorn/celery/feeds/tasks.py
+++ akorn/celery/feeds/tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 from akorn.celery.scrapers.tasks import scrape_journal
-import feed_handlers
+from akorn.scrapers import scrapers
 import feedparser
 
 from akorn.celery.couch import db_store
@@ -8,14 +8,19 @@ from akorn.celery.celeryconfig import BROKER_URL, CELERY_RESULT_BACKEND
 
 app = Celery('feed_tasks', broker=BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
-@app.task('fetch-feed')
-def fetch_feed(feedhandler, feed_urls):
-    for feed_url in feed_urls:
-        add_feed_items.delay(feedhandler, feed_url)
+scraper_factory = scrapers.Scrapers()
 
-def fix_url(url):
-  # Remove session key for wiley urls
-  return url.split(';')[0]
+@app.task('fetch-feeds')
+def fetch_feed(scraper_module=None):
+    if scraper_module is not None:
+        feedhandler, feed_urls = scraper_factory.feeds[scraper_module]
+        for feed_url in feed_urls:
+            add_feed_items.delay(feedhandler, feed_url)
+    else:
+        for scraper_module, (feedhandler, feed_urls) in scraper_factory.feeds.items():
+            for feed_url in feed_urls:
+                add_feed_items.delay(feedhandler, feed_url)
+
 
 @app.task('add-feed-items')
 def add_feed_items(feedhandler, feed_url):
@@ -24,18 +29,10 @@ def add_feed_items(feedhandler, feed_url):
     # should be smarter here, e.g. use If-Modified-Since
     feed = feedparser.parse(feed_url, agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50')
 
-    if feedhandler in feed_handlers.handlers:
-        handler = feed_handlers.handlers[feedhandler]
-    else:
-        handler = feed_handlers.handlers['default']
-
     for item in feed['items']:
-        if "abstract" in handler:
-            base_article = {'abstract':handler['abstract'](item)}
-        else:
-            base_article = {}
+        base_article = {}
 
-        item_url = fix_url(handler['url'](item))
+        item_url = item[feedhandler]
 
         check_exists = db_store.view('index/sources', key=item_url, include_docs='false')
 
